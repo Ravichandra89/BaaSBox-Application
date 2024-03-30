0a1c00f841c0965176393835c48aa31eb51d9b79
Ravi chandra <githubravi01@gmail.com>
2024-03-31 00:58:09 +0530

    Add server controllers and routes, update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 mongoose
-  .connect('mongodb+srv://ravi:ravi1234@mycluster.niru1ou.mongodb.net/?retryWrites=true&w=majority&appName=mycluster')
+  .connect(process.env.MONGO)
   .then(async () => {
     await console.log("DataBase Connected");
   })
@@ -21,3 +22,5 @@ const app = express();
 app.listen(PORT, () => {
   console.log("Server Running on 3000!! ");
 });
+
+app.use('/api/users', userRouter);
