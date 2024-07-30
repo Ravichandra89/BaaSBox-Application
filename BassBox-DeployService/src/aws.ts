d07f4a5673c6c23e293480a805f55c2a64e65ef7
Ravi chandra <githubravi01@gmail.com>
2024-07-30 21:55:45 +0530

    Phase 2 (Getting Code and Building and put Again on s3)

--- BassBox-DeployService/src/aws.ts
+++ BassBox-DeployService/src/aws.ts
@@ -11,41 +11,95 @@ const s3 = new S3({
 
 // Function to download files from s3
 export async function downloadS3Folder(prefix: string) {
-  const allFiles = await s3
-    .listObjectsV2({
-      Bucket: "vercel",
-      Prefix: prefix,
-    })
-    .promise();
+  try {
+    const allFiles = await s3
+      .listObjectsV2({
+        Bucket: "vercel",
+        Prefix: prefix,
+      })
+      .promise();
 
-  const allPromises =
-    allFiles.Contents?.map(({ Key }) => {
-      return new Promise((resolve, reject) => {
-        if (!Key) {
-          resolve("");
-          return;
-        }
+    const allPromises =
+      allFiles.Contents?.map(({ Key }) => {
+        return new Promise((resolve, reject) => {
+          if (!Key) {
+            resolve("");
+            return;
+          }
 
-        const localFilePath = path.join(__dirname, Key);
-        const outputFile = fs.createWriteStream(localFilePath);
-        const dirName = path.dirname(localFilePath);
+          const localFilePath = path.join(__dirname, Key);
+          const outputFile = fs.createWriteStream(localFilePath);
+          const dirName = path.dirname(localFilePath);
 
-        if (!fs.existsSync(dirName)) {
-          fs.mkdirSync(dirName, { recursive: true });
-        }
+          if (!fs.existsSync(dirName)) {
+            fs.mkdirSync(dirName, { recursive: true });
+          }
 
-        s3.getObject({
-          Bucket: "vercel",
-          Key,
-        })
-          .createReadStream()
-          .pipe(outputFile)
-          .on("finish", () => resolve(""))
-          .on("error", (err) => reject(err));
-      });
-    }) || [];
+          s3.getObject({
+            Bucket: "vercel",
+            Key,
+          })
+            .createReadStream()
+            .pipe(outputFile)
+            .on("finish", () => resolve(""))
+            .on("error", (err) => reject(err));
+        });
+      }) || [];
 
-  console.log("Awaiting");
+    console.log("Awaiting");
 
-  await Promise.all(allPromises.filter((x) => x != undefined));
+    await Promise.all(allPromises.filter((x) => x !== undefined));
+  } catch (error) {
+    console.error("Error while downloading files from S3", error);
+  }
 }
+
+// Function to get all files
+const getAllFiles = (folderPath: string): string[] => {
+  let response: string[] = [];
+  try {
+    const allFilesAndFolders = fs.readdirSync(folderPath);
+    allFilesAndFolders.forEach((file) => {
+      const fullFilePath = path.join(folderPath, file);
+      if (fs.statSync(fullFilePath).isDirectory()) {
+        response = response.concat(getAllFiles(fullFilePath));
+      } else {
+        response.push(fullFilePath);
+      }
+    });
+  } catch (error) {
+    console.error("Error while getting all files", error);
+  }
+  return response;
+};
+
+// Function to copy the files from BuildProject process
+export async function copyFilesFromBuildProjectModule(id: string) {
+  try {
+    const folderPath = path.join(__dirname, `output/${id}/dist`);
+    const allFiles = getAllFiles(folderPath);
+    allFiles.forEach((file) => {
+      uploadFile(`dist/${id}` + file.slice(folderPath.length + 1), file);
+    });
+  } catch (error) {
+    console.error("Error while copying files", error);
+  }
+}
+
+// Function to upload on s3
+const uploadFile = async (fileName: string, localFilePath: string) => {
+  try {
+    const fileContent = fs.readFileSync(localFilePath);
+    await s3
+      .upload({
+        Body: fileContent,
+        Bucket: "vercel",
+        Key: fileName,
+      })
+      .promise();
+  } catch (error) {
+    console.error("Error while uploading BuildProject files to S3", error);
+  }
+};
+
+export default getAllFiles;
--- BassBox-DeployService/src/index.ts
+++ BassBox-DeployService/src/index.ts
@@ -1,14 +1,37 @@
 import { createClient, commandOptions } from "redis";
-import { copyFinalDist, downloadS3Folder } from "./aws";
-import { BuildProject } from "./utils";
+import { copyFilesFromBuildProjectModule, downloadS3Folder } from "./aws";
+import { BuildTheProject } from "./utils";
+const subscriber = createClient();
+subscriber.connect();
+
+const publisher = createClient();
+publisher.connect();
 
 async function main() {
   while (true) {
-    const value = await subscriber.brPop(
-        commandOptions({isolated: true}),
+    try {
+      const res = await subscriber.brPop(
+        commandOptions({ isolated: true }),
         "build-queue",
         0
-    );
+      );
 
+      if (res && res.element) {
+        const id = res.element;
+
+        await downloadS3Folder(`output/${id}`);
+        await BuildTheProject(id);
+        copyFilesFromBuildProjectModule(id);
+        publisher.hSet("status", id, "deployed");
+      } else {
+        console.log("No jobs in queue");
+      }
+      
+    } catch (error) {
+      console.error("Error processing Build Queue", error);
+    }
   }
 }
+
+// Running Infinitly?
+main();
