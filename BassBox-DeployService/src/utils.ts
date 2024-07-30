019dbdf88166877f6f76a328b471974c663b02ec
Ravi chandra <githubravi01@gmail.com>
2024-07-30 17:36:51 +0530

    Project Build child Process (Deployment 1 Phase)

--- BassBox-DeployService/src/utils.ts
+++ BassBox-DeployService/src/utils.ts
@@ -1,1 +1,22 @@
 // Logic to convert things into HTML/ Css / JS
+
+import { spawn, exec } from "child_process";
+import path from "path";
+
+export function BuildTheProject(id: string) {
+  const child = exec(
+    `cd ${path.join(__dirname, `output/${id}`)} npm install && npm run build`
+  );
+
+  child.stdout?.on("data", function (data) {
+    console.log("stdout" + data);
+  });
+
+  child.stderr?.on("data", function (data) {
+    console.log("stderr" + data);
+  });
+
+  child.on("close", function (code) {
+    path.resolve("");
+  });
+}
